13b87084669e512b931f212e1a49e729bc4aa9da
Uroro C. Obaji <ucobaji@secnigeria.org>
2022-10-30 21:16:23 +0100

    First commit

---
+++ assignment.py
@@ -0,0 +1,65 @@
+#String:
+'Uroro'
+"Nigeria"
+'Python'
+print('Uroro')
+
+#Integers
+a = 10
+b = 5
+c = -20
+print(a)
+
+#Booleans
+20 < 50
+10 > 3
+5 == 7
+print(5 == 7)
+
+#Complex numbers
+m = 7j
+n = -10j
+o = 13j
+print(o)
+
+#Float
+x = 5.3
+y = -7.5
+z = 6.9
+print(z)
+
+#List
+myjewelry = ['necklace', 'earring' 'earring', 'bracelet']
+theboys = ['Ogechukwu', 'Somtochukwu', 'Ebubechukwu', 'Obimdinachi']
+redfruits = ['strawberries', 'cherries', 'plums']
+print(theboys)
+
+#Tuple
+thecandidates = ('Tinubu', 'Obi', 'Atiku')
+thecities = ('Lagos', 'Lagos', 'Abuja', 'Portharcourt')
+myseasons = ('winter', 'spring', 'autum', 'summer')
+print(thecities)
+
+#Dictionaries
+mymum = {
+    'surname': 'Obaji',
+    'firstname': 'Joy',
+    'middlename': 'Ihuoma'
+}
+myplace = {
+    'state': 'Imo',
+    'LGA': 'Owerri',
+    'Village': 'Emii'
+}
+thiscountry = {
+    'name': 'Nigeria',
+    'capital': 'FCT',
+    'independence': '1960'
+}
+print(mymum)
+
+#Sets
+thesubjects = {'mathematics', 'english', 'economics'}
+primarycolours = {'red', 'yellow', 'blue'}
+evennumbers = {2, 4, 6, 8, 10}
+print(evennumbers)
---
+++ project.py
@@ -0,0 +1,39 @@
+#INSTRUCTIONS:
+#Build a python password generator
+#Push it to your github account
+#Create a repository for this
+#Share the link on the telegram channel
+
+#SOLUTION:
+
+# import secrets and string
+import secrets
+import string
+
+# define the alphabet
+letters = string.ascii_letters
+digits = string.digits
+special_chars = string.punctuation
+
+alphabet = letters + digits + special_chars
+
+# choose password length
+pwd_length = 16
+
+# generate a password string
+pwd = ''
+for i in range(pwd_length):
+  pwd += ''.join(secrets.choice(alphabet))
+
+print(pwd)
+
+# generate password meeting constraints
+while True:
+  pwd = ''
+  for i in range(pwd_length):
+    pwd += ''.join(secrets.choice(alphabet))
+
+  if (any(char in special_chars for char in pwd) and 
+      sum(char in digits for char in pwd)>=2):
+          break
+print(pwd)
